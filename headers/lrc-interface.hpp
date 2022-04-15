d1550a8a24990eb405615212f93274aa5f1f6f4c
etrian-dev <nicola.vetrini@gmail.com>
2022-04-15 12:52:10 +0200

    Added interface class header

---
+++ headers/lrc-interface.hpp
@@ -0,0 +1,43 @@
+#ifndef LRC_INTERFACE_INCLUDED
+#define LRC_INTERFACE_INCLUDED
+
+#include <string>
+#include <vector>
+#include <tuple>
+// ncurses header
+#include <ncurses.h>
+
+using std::string;
+using std::vector;
+
+class Lrc_interface {
+private:
+	// Generator instance reference
+	Lrc_generator *model;
+	// TUI functions & variables
+	WINDOW *menu;
+	WINDOW *lyrics_win;
+	int max_height, max_width;
+	// menu items (updated dynamically by the model)
+	vector<std::tuple<string, string>> menu_items;
+
+	
+	void interface_setup(void);
+	// utility function to draw the menu
+	void draw_menu(void);
+	// draw content
+	void draw_content();
+	// creates a dialog to set the chosen attribute
+	void set_attr_dialog(string attr);
+	// displays a simple choiche dialog
+	char choice_dialog(string msg);
+public:
+	// runs the interface
+	void run(void);
+	
+	Lrc_interface(Lrc_generator& model);
+	~Lrc_interface();
+	
+};
+
+#endif
